d28b4740d0fa68d1cb7fc87dfd6b6e94943eedfa
Jose Berrio <aldarisbm@gmail.com>
2019-01-10 09:52:24 -0600

    changes

--- routes/slackbot.js
+++ routes/slackbot.js
@@ -2,8 +2,16 @@ require('dotenv').config
 var express = require('express');
 var router = express.Router();
 
+
+router.post('/challenge',(req, res)=>{
+  res.send(req.body.challenge);
+})
+
 router.post('/', (req, res)=>{
   console.log(req.body)
 })
 
+router.post('/hello', (req, res)=>{
+  res.send(req.body)
+})
 module.exports = router;
