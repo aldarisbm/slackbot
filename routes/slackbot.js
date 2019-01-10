e333ec98226cc272b99651948c72b0d2546b2d11
Jose Berrio <aldarisbm@gmail.com>
2019-01-10 10:50:03 -0600

    working changes

--- routes/slackbot.js
+++ routes/slackbot.js
@@ -3,16 +3,17 @@ var express = require('express');
 var router = express.Router();
 
 
-router.post('/challenge',(req, res)=>{
-  res.send(req.body.challenge);
+router.post('/',(req, res)=>{
+  if(req.body.challenge && req.body.type === 'url_verification'){
+    return res.send(req.body.challenge);
+  }
+  console.log(req.body)
+  res.send('Hello Sir')
 })
 
-router.post('/', (req, res)=>{
-  console.log(req.body)
-})
 
-router.post('/hello', (req, res)=>{
-  console.log(req.body)
-  res.send('hello')
+router.post('/skills', (req, res)=>{
+  console.log(req.body.text)
+  res.send(`Hello ${req.body.user_name} I'm not sure what your skills are`)
 })
 module.exports = router;
